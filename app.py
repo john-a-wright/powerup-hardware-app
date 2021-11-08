6eafa28af14127b932b1746304b153bd78a1f48e
john-a-wright <johnawright@utexas.edu>
2021-11-08 13:07:20 -0600

    api attemps

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask.helpers import send_from_directory
 app = Flask(__name__, static_folder="frontend/build", static_url_path="")
 
 # comment out when building for server
-#from flask_cors import CORS
-#CORS(app)
+from flask_cors import CORS
+CORS(app)
 
 # Get the name from the url and return the output
-@app.route('/dashboard/<string:name>',methods=['GET'])
-def output_name(name):  # put application's code here
-    if name=='John':
+@app.route('/api/dashboard/<string:name>', methods=['GET'])
+def output_name(name):
+    if name == 'John':
         return jsonify(
             status=200,
             message="Wright"
@@ -22,10 +22,19 @@ def output_name(name):  # put application's code here
             message="User Not Found"
         )
 
+# return hardware sets
+@app.route('/api/dashboard/hardware', methods=['GET'])
+def get_sets():
+    return jsonify(
+        status=200,
+        message="test hardware set"
+    )
+
 # Starting route for index
 @app.route("/")
 def index():
     return send_from_directory(app.static_folder, "index.html")
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
@@ -8,6 +8,8 @@ export default function Dashboard() {
     const [error, setError] = useState()
     const {currentUser, logout} = useAuth()
     const { history } = useHistory()
+
+    const [currentSets, setCurrentSets] = React.useState("");
 
     async function handleLogout() {
         setError('')
@@ -19,6 +21,21 @@ export default function Dashboard() {
         }
     }
 
+    const getCurrentSets = () => {
+        const url = process.env.REACT_APP_URL_PREFIX + "/api/dashboard/hardware";
+        console.log(url);
+        fetch(url)
+          .then((response) => response.json())
+          .then((jsondata) => setCurrentSets(jsondata["message"]))
+          .catch((err) => {
+            console.log(err);
+        });
+    };
+
+    useEffect(() => {
+        getCurrentSets();
+    }, []);
+
     return (
         <>
             <Card>
@@ -28,10 +45,17 @@ export default function Dashboard() {
                     <strong> Email: </strong>{currentUser.email}
                     <Link to="/update-profile" className="btn btn-primary w-100 mt-3">Update Profile</Link>
                 </Card.Body>
-
             </Card>
             <div className="w-100 text center mt-2">
-                <Button variant="link" onClick={handleLogout}>Log Out</Button> 
+                <Button variant="link" onClick={handleLogout}>Log Out</Button>
+            </div>
+            <div>
+                <h1>
+                    Current Hardware Sets:
+                </h1>
+                <p>
+                    {currentSets}
+                </p>
             </div>
         
         </>
