9f3780e3172790488ea47316e75dcf799ddabcf4
john-a-wright <johnawright@utexas.edu>
2021-11-08 19:24:25 -0600

    color fix 2

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -44,7 +44,7 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
+            <div>
 
                 <h1>
                     PowerUp Hardware
